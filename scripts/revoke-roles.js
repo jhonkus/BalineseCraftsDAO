bb1259550251d7fd5320937037dc022863b609f9
putukus <putu@codekiwi.com>
2022-10-25 20:34:00 +0800

    add revoke script

---
+++ scripts/revoke-roles.js
@@ -0,0 +1,27 @@
+import sdk from "./initialize-sdk.js";
+
+// This is our ERC-20 contract.
+const token = await sdk.getContract(
+    "0xDf5E56C72FA0379EAFa8d743571C98Dd2eD28835",
+    "token"
+  );
+
+(async () => {
+  try {
+    // Log the current roles.
+    const allRoles = await token.roles.getAll();
+
+    console.log("👀 Roles that exist right now:", allRoles);
+
+    // Revoke all the superpowers your wallet had over the ERC-20 contract.
+    await token.roles.setAll({ admin: [], minter: [] });
+    console.log(
+      "🎉 Roles after revoking ourselves",
+      await token.roles.getAll()
+    );
+    console.log("✅ Successfully revoked our superpowers from the ERC-20 contract");
+
+  } catch (error) {
+    console.error("Failed to revoke ourselves from the DAO treasury", error);
+  }
+})();
